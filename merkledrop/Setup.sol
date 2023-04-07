a16813de6c6e680babf17406a36f341b480765eb
arielhqz <haoqing_zhou@163.com>
2023-04-07 20:18:23 +0800

    Create Setup.sol
    题目

---
+++ merkledrop/Setup.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.16;
+
+import "./MerkleDistributor.sol";
+
+contract Token is ERC20Like {
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    uint256 public totalSupply = 1_000_000 ether;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function approve(address to, uint256 amount) public returns (bool) {
+        allowance[msg.sender][to] = amount;
+        return true;
+    }
+
+    function transfer(address to, uint256 amount) public returns (bool) {
+        return transferFrom(msg.sender, to, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
+        if (from != msg.sender) {
+            allowance[from][to] -= amount;
+        }
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        return true;
+    }
+}
+
+contract Setup {
+
+    Token public immutable token;
+    MerkleDistributor public immutable merkleDistributor;
+
+    constructor() payable {
+        token = new Token();
+        uint256 airdropAmount = 75000 * 10 ** 18;
+        // 凭空产生75000个代币
+        merkleDistributor = new MerkleDistributor(
+            // import的合约
+            address(token), 
+            bytes32(0x5176d84267cd453dad23d8f698d704fc7b7ee6283b5131cb3de77e58eb9c3ec3)
+        );
+        token.transfer(address(merkleDistributor), airdropAmount);
+        // 把空投产生的全部代币打入merkleDistributor的地址
+    }
+
+    function isSolved() public view returns (bool) {
+        bool condition1 = token.balanceOf(address(merkleDistributor)) == 0;
+        bool condition2 = false;
+        for (uint256 i = 0; i < 64; ++i) {
+        // 在tree.json文档中，编号0-63，共64个领取者，要求有一个领取者没有领取
+        // 该json文档领取者amount加总正好为75000
+        // 所以题目就是即希望代币被领完，又希望有人没有领
+            if (!merkleDistributor.isClaimed(i)) {
+                condition2 = true;
+                break;
+            }
+        }
+        return condition1 && condition2;
+    }
+}
